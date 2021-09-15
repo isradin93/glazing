8323f67fee663bf5f8425d5b4dbdb9e1716bcfb0
Isradin <isradin.nurbek@gmail.com>
2021-09-15 20:27:14 +0300

    Create popup modal dialog

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,16 +1,18 @@
 const modals = () => {
-    const bindModal = (triggerSelector, modalSelector, closeBtnSelector) => {
+    const bindModal = (triggerSelector, modalSelector, closeSelector) => {
+
         const triggers = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
-            closeBtn = document.querySelector(closeBtnSelector);
+            closeBtn = document.querySelector(closeSelector);
 
         triggers.forEach(trigger => {
-            trigger.addEventListener('click', (e) => {
+            trigger.addEventListener('click', e => {
                 if (e.target) {
                     e.preventDefault();
                 }
 
                 modal.style.display = 'block';
+                modal.classList.add('animated', 'fadeIn');
                 document.body.style.overflow = 'hidden';
             });
         });
@@ -20,24 +22,14 @@ const modals = () => {
             document.body.style.overflow = '';
         });
 
-        modal.addEventListener('click', (e) => {
-            if (e.target === modal) {
-                modal.style.display = 'none';
-                document.body.style.overflow = '';
-            }
+        modal.addEventListener('click', () => {
+            modal.style.display = 'none';
+            document.body.style.overflow = '';
         });
-    };
-
-    const showModalByTimeout = (selector, timeout) => {
-        setTimeout(() => {
-            document.querySelector(selector).style.display = 'block';
-            document.body.style.overflow = 'hidden';
-        }, timeout);
     };
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
-    //showModalByTimeout('.popup', 60000);
 };
 
 export default modals;
